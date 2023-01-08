bdb191eaa467533457e52754812cbdc2f495cbc9
ydeezly <ydglitch@gmail.com>
2023-01-08 07:56:04 +0000

    upd: main

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from typing import Union
-from tortoise import Tortoise
 from fastapi import FastAPI
-from db import init_db
+
 from tortoise import run_async
 
 app = FastAPI()
-
-run_async(init_db())
 
 @app.get("/")
 def read_root():
@@ -16,3 +13,5 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
