17cab48aaffcdd90d27ca6f927be841c292114d0
ydeezly <ydglitch@gmail.com>
2023-01-08 21:29:38 +0000

    upd: db

--- db.py
+++ db.py
@@ -3,6 +3,7 @@ import logging
 from tortoise import Tortoise, run_async
 from dotenv import load_dotenv
 import models.models as models
+import time
 
 load_dotenv()
 db = os.getenv("DB_URL")
@@ -17,8 +18,8 @@ async def init_db():
             }
         )
         await Tortoise.generate_schemas()
-        logging.info("database initialized")
-        print("init db")
+        logging.info("database initialized succesfully.")
+        print("initialized db")
     except Exception as e:
         logging.error("there was an error initializing db")
         print(e)
