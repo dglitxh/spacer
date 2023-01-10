c97dd8041517aabd1fa587f6daaab642f674d069
ydeezly <ydglitch@gmail.com>
2023-01-10 20:03:23 +0000

    upd: main

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from typing import Union
 from fastapi import FastAPI
-
-from tortoise import run_async
+import datetime
+from models.schema import Ticket
+import models.models as models
+from tortoise.contrib.pydantic import pydantic_model_creator
+from common.db import init_db
 
 app = FastAPI()
+init_db(app)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/post")
+async def read_item(ticket: Ticket):
+    print(ticket)
+    tick = await models.Ticket.create(**ticket.dict())
+    print(post)
+    return tick
 
 
