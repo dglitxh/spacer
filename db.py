117c342ea4696e58b880e733f5cdb16a3dbef866
ydeezly <ydglitch@gmail.com>
2023-01-10 03:55:59 +0000

    upd: db

--- db.py
+++ db.py
@@ -3,25 +3,27 @@ import logging
 from tortoise import Tortoise, run_async
 from dotenv import load_dotenv
 import models.models as models
+from tortoise.contrib.fastapi import HTTPNotFoundError, register_tortoise
 import time
 
 load_dotenv()
-db = os.getenv("DB_URL")
+db = os.getenv("DB_URL") 
 
-async def init_db():
+def init_db(app):
     try:
         print("initializing database")
-        await Tortoise.init(
+        register_tortoise(
+            app,
             db_url=db,
             modules = {
                 "models": [models]
-            }
+            },
+            generate_schemas=True,
+            add_exception_handlers=True,
         )
-        await Tortoise.generate_schemas()
         logging.info("database initialized succesfully.")
         print("initialized db")
     except Exception as e:
         logging.error("there was an error initializing db")
         print(e)
 
-run_async(init_db())
