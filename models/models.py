e4c4e7006ca90e6081494e9459b4b49685f8107d
ydeezly <ydglitch@gmail.com>
2023-01-12 05:31:40 +0000

    fix: models

--- models/models.py
+++ models/models.py
@@ -3,16 +3,16 @@ from tortoise import fields
 
 
 class User(Model):
-    id: fields.IntField(pk=True, generated=True)
-    firstname: fields.CharField(max_length=255, null=False)
-    lastname: fields.CharField(max_length=255)
-    age: fields.IntField()
-    email: fields.CharField(max_length=255, unique=True, null=False)
-    user_type: fields.CharField(max_length=255)
-    gender: fields.CharField(max_length=255)
-    password: fields.CharField(max_length=255, null=False)
-    created_at: fields.DatetimeField(auto_now_add=True)
-    modified_at: fields.DatetimeField(auto_now=True)
+    id = fields.IntField(pk=True, generated=True)
+    firstname = fields.CharField(max_length=255, null=False)
+    lastname = fields.CharField(max_length=255)
+    age = fields.IntField()
+    email = fields.CharField(max_length=255, unique=True, null=False)
+    user_type = fields.CharField(max_length=255)
+    gender = fields.CharField(max_length=255)
+    password = fields.CharField(max_length=255, null=False)
+    created_at = fields.DatetimeField(auto_now_add=True)
+    modified_at = fields.DatetimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -20,12 +20,12 @@ class User(Model):
         table: "users"
 
 class Ticket(Model):
-    id: fields.IntField(pk=True, generated=True)
-    event_id: fields.ForeignKeyField("models.Event", related_name="event")
-    user_id: fields.ForeignKeyField("models.User", related_name="owner")
-    ticket_type: fields.CharField(max_length=255)
-    created_at: fields.DatetimeField(auto_now_add=True)
-    modified_at: fields.DatetimeField(auto_now=True)
+    id = fields.IntField(pk=True, generated=True)
+    event_id = fields.ForeignKeyField("models.Event", related_name="event")
+    user_id = fields.ForeignKeyField("models.User", related_name="owner")
+    ticket_type = fields.CharField(max_length=255)
+    created_at = fields.DatetimeField(auto_now_add=True)
+    modified_at = fields.DatetimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -34,12 +34,12 @@ class Ticket(Model):
         table: "tickets"
 
 class Event(Model):
-    id: fields.IntField(pk=True, generated=True)
-    tickets: fields.ForeignKeyField("models.Ticket", related_name="tickets")
-    user_id: fields.ForeignKeyField("models.User", related_name="owner")
-    ticket_type: fields.CharField(max_length=255)
-    created_at: fields.DatetimeField(auto_now_add=True)
-    modified_at: fields.DatetimeField(auto_now=True)
+    id = fields.IntField(pk=True, generated=True)
+    tickets = fields.ForeignKeyField("models.Ticket", related_name="tickets")
+    user_id = fields.ForeignKeyField("models.User", related_name="owner")
+    ticket_type = fields.CharField(max_length=255)
+    created_at = fields.DatetimeField(auto_now_add=True)
+    modified_at = fields.DatetimeField(auto_now=True)
 
     def __str__(self):
         return self.name
