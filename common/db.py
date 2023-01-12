bc4b71f4d5e36cfa9890ac085c90465f3d79c331
ydeezly <ydglitch@gmail.com>
2023-01-12 05:31:58 +0000

    upd: db.py

---
+++ common/db.py
@@ -0,0 +1,38 @@
+import os
+import logging
+from tortoise import Tortoise, run_async
+from dotenv import load_dotenv
+import models.models as models
+from tortoise.contrib.fastapi import HTTPNotFoundError, register_tortoise
+import time
+
+load_dotenv()
+db = os.getenv("DB_URL") 
+
+def init_db(app):
+    try:
+        print("initializing database")
+        register_tortoise(
+            app,
+            db_url=db,
+            modules = {
+                "models": [models, "aerich.models"]
+            },
+            generate_schemas=True,
+            add_exception_handlers=True,
+        )
+        logging.info("database initialized succesfully.")
+        print("initialized db")
+    except Exception as e:
+        logging.error("there was an error initializing db")
+        print(e)
+
+TORTOISE_ORM = {
+    "connections": {"default": db},
+    "apps": {
+        "models": {
+            "models": [models, "aerich.models"],
+            "default_connection": "default",
+        },
+    },
+}
