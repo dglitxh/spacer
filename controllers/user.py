38fb53e42523e877f001fef66150c422a4a3047d
ydeezly <ydglitch@gmail.com>
2023-01-13 07:43:21 +0000

    upd: user signup

--- controllers/user.py
+++ controllers/user.py
@@ -1,8 +1,25 @@
 from ..models import models, schema
+from passlib.context import CryptContext
 import logging
 
-async def signup(creds: models.User) -> schema.User:
-    user = await models.User.create(creds)
-    return user
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-    
+def hasher(password: str) -> str:
+    hashed = pwd_context.hash(password)
+    return hashed
+
+
+async def signup(creds: models.User) -> schema.User:
+    try:
+        hashed_pass = hasher(creds.password)
+        creds.password = hashed_pass
+        user = await models.User.create(creds)
+        return user
+    except Exception as e:
+        # need to create universal logger.
+        logging.error("There was an error creating user")
+        print(e)
+
+
+
+
