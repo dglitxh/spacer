08b9abbc843536d9dace7d9c9df631a49ce289ed
ydeezly <ydglitch@gmail.com>
2023-01-18 16:22:30 +0000

    upd: auth tests

--- tests/auth_tests.py
+++ tests/auth_tests.py
@@ -2,3 +2,10 @@ import httpx
 from ..main import app
 from fastapi.testclient import TestClient
 
+
+client = TestClient(app)
+
+
+def test_signup():
+    response = client.get("/signup")
+    assert response.status_code == 200
