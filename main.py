c5aa0b79dc81d5d781b3c07e65285b4fc276ce23
ydeezly <ydglitch@gmail.com>
2023-01-20 21:56:29 +0000

    upd: main

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 from typing import Union
 from fastapi import FastAPI
 import datetime
-from models.schema import Ticket
-import models.models as models
+from .models.schema import Ticket
+from .models import models
 from tortoise.contrib.pydantic import pydantic_model_creator
-from common.db import init_db
-from common.logger import logger
-from controllers.user_controller import auth_router
+from .common.db import init_db
+from .common.logger import logger
+from .controllers.user_controller import router as auth_router
 
 app = FastAPI()
 app.include_router(auth_router)
 init_db(app)
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
+async def home():
+    return {"msg: get me lit"}
+    
 @app.post("/post")
 async def read_item(ticket: Ticket):
     print(ticket)
