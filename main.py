f7a2ca3938bbc16d2559e2ddc5607e28545f3c99
ydeezly <ydglitch@gmail.com>
2023-01-21 00:17:28 +0000

    fix: main

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from typing import Union
 from fastapi import FastAPI
 import datetime
-from .models.schema import Ticket
-from .models import models
+from models.schema import Ticket
+from models import models
 from tortoise.contrib.pydantic import pydantic_model_creator
-from .common.db import init_db
-from .common.logger import logger
-from .controllers.user_controller import router as auth_router
+from common.db import init_db
+from common.logger import logger
+from controllers.user_controller import router as auth_router
 
 app = FastAPI()
 app.include_router(auth_router)
