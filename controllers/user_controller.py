6830a5de478e453cdca4de02a92267845c2e2eb4
ydeezly <ydglitch@gmail.com>
2023-01-26 07:56:33 +0000

    upd: auth

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -17,6 +17,25 @@ def hasher(password: str) -> str:
     hashed = pwd_context.hash(password)
     return hashed
 
+async def get_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, os.getenv("SECRET_KEY"), algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
     if expires_delta:
