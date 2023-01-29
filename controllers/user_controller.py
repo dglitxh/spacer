10d613e702033817e88710968e5a23682dad4ea3
ydeezly <ydglitch@gmail.com>
2023-01-29 20:57:23 +0000

    upd: auth

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -1,3 +1,5 @@
+import os
+import json
 from models import models, schema
 from passlib.context import CryptContext
 from common.logger import logger
@@ -15,15 +17,13 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 router = APIRouter(prefix="/auth")
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def client_user(client_user, user):
-    # client_user.firstname = user.firstname
-    # client_user.lastname = user.lastname
-    # client_user.age = user.age
-    # client_user.gender = user.gender
-    # client_user.email = user.email
-    for i in user:
-        if client_user[i]:
-            client_user[i] = user[i]
+def client_user(client_user: schema.ClientUser, user):
+    client_user.firstname = user.firstname
+    client_user.lastname = user.lastname
+    client_user.age = user.age
+    client_user.gender = user.gender
+    client_user.email = user.email
+    client_user.user_type = user.user_type
     return client_user
 
 
@@ -31,6 +31,7 @@ def hasher(password: str) -> str:
     hashed = pwd_context.hash(password)
     return hashed
 
+@router.get("/get_user")
 async def get_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
@@ -39,21 +40,19 @@ async def get_user(token: str = Depends(oauth2_scheme)):
     )
     try:
         payload = jwt.decode(token, os.getenv("SECRET_KEY"), algorithms=[ALGORITHM])
-        email: str = payload.get("creds")
+        email = payload["creds"]
         if email is None:
             raise credentials_exception
-        token_data = TokenData(email=email)
     except JWTError:
         raise credentials_exception
-    user = user = models.User.get_or_none(email=token_data.email)
+    user = await models.User.get_or_none(email=email)
     if user is None:
         raise credentials_exception
-    client: schema.ClientUser = {}
-    output_client = client_user(client, user)
-    return output_client
+    client = client_user(client_user, user)
+    return client
 
 
-def create_access_token(data: dict, expires_delta: timedelta):
+async def create_access_token(data: dict, expires_delta: timedelta):
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
@@ -61,7 +60,7 @@ def create_access_token(data: dict, expires_delta: timedelta):
         expire = datetime.utcnow() + timedelta(minutes=30)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, os.getenv("SECRET_KEY"), algorithm=ALGORITHM)
-    cache_token = rdb.set("token", encoded_jwt)
+    cache_token = await rdb.set("token", encoded_jwt)
     logger.info(encoded_jwt)
     return encoded_jwt
 
@@ -73,7 +72,7 @@ async def signup(creds: schema.User) -> schema.User:
         creds.password = hashed_pass
         user = await models.User.create(**creds.dict())
         logger.info("User signed up succesfully.")
-        token = create_access_token(data={"creds": user.email}, expires_delta=30)
+        token = create_access_token(data={"creds": user.email}, expires_delta=timedelta(30))
         return user
     except Exception as e:
         logger.error("There was an error creating user")
@@ -83,18 +82,22 @@ async def signup(creds: schema.User) -> schema.User:
 
 @router.post("/login", summary="Authenticate user")
 async def login(creds: schema.Login) -> schema.ClientUser:
-    try:
-        cred_pass = hasher(creds.password)
-        user = models.User.get_or_none(email=creds.email)
-        verify = pwd_context.verify(cred_pass, user.password)
-        if not user:
-            raise HTTPException(
+    http_exception = HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="Incorrect username or password",
                 headers={"WWW-Authenticate": "Bearer"},
             )
-        token = create_access_token(data={"creds": user.email}, expires_delta=30)
-        return user
+    try:
+        user: schema.ClientUser = await models.User.get_or_none(email=creds.email)
+        verify = pwd_context.verify(creds.password, user.password)
+        print(verify)
+        if not verify:
+            raise http_exception
+        token = await create_access_token(data={"creds": user.email}, expires_delta=timedelta(minutes=30))
+        client = client_user(client_user, user)
+        return client
     except Exception as e:
         logger.error("There was an error authenticating this user.")
         print(e)
+        raise http_exception
+       
