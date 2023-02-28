638520502d4ef9c5cb3cca02cb5cc1c9a7b03d7c
ydeezly <ydglitch@gmail.com>
2023-02-28 23:07:46 +0000

    upd: nav

--- controllers/nav_controller.py
+++ controllers/nav_controller.py
@@ -2,6 +2,5 @@ import os
 import json
 from models import models, schema
 from fastapi import Depends, FastAPI, HTTPException, APIRouter, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from dotenv import load_dotenv
 
