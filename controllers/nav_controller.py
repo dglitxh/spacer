c3d6608c035d78d4c4e58b9af1420cf6b489fe56
ydeezly <ydglitch@gmail.com>
2023-03-03 10:45:52 +0000

    upd: create store

--- controllers/nav_controller.py
+++ controllers/nav_controller.py
@@ -4,6 +4,7 @@ from models import models, schema
 from fastapi import Depends, FastAPI, HTTPException, APIRouter, status
 from dotenv import load_dotenv
 
+router = APIRouter(prefix="/store")
 
 @router.post("/new", summary="Create an account")
 async def add_store(data: schema.Store) -> schema.Store:
