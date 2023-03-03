4de95d3a746db8ca610df412eddbb2a5f5391f1a
ydeezly <ydglitch@gmail.com>
2023-03-03 10:40:58 +0000

    upd: models

--- models/models.py
+++ models/models.py
@@ -42,10 +42,11 @@ class CartItem(Model):
     product: fields.ForeignKeyField(model_name=Product)
     quantity: fields.IntField()
     total_price: fields.FloatField()
+    
 class Order(Model): 
     id: fields.IntField(pk=True, generated=True)
     user: fields.ForeignKeyField(model_name=User, related_name="user")
-    code: fields.UUIDField(generated=True)
+    order_code: fields.UUIDField(generated=True)
     paid: fields.BooleanField(default=False)
     delivery: fields.BooleanField(default=False)
     items: fields.ForeignKeyField(model_name=CartItem, related_name="cart_items")
