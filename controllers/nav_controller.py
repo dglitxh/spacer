294ad3f074fec3ecf60c4d1ec2c7df50cec7e8d5
ydeezly <ydglitch@gmail.com>
2023-03-03 10:44:38 +0000

    add: create store

--- controllers/nav_controller.py
+++ controllers/nav_controller.py
@@ -4,3 +4,18 @@ from models import models, schema
 from fastapi import Depends, FastAPI, HTTPException, APIRouter, status
 from dotenv import load_dotenv
 
+
+@router.post("/new", summary="Create an account")
+async def add_store(data: schema.Store) -> schema.Store:
+    http_exception = HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Failed to create store.",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    try:
+        store = await models.User.create(**data.dict())
+        logger.info("store created succesfully")
+        return client
+    except Exception as e:
+        logger.error(e)
+        raise http_exception
