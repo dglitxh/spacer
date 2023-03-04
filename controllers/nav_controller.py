3b05ad89f4620e274afc83cff77c508ef571a499
ydeezly <ydglitch@gmail.com>
2023-03-04 16:44:44 +0000

    add: get store

--- controllers/nav_controller.py
+++ controllers/nav_controller.py
@@ -21,6 +21,20 @@ async def add_store(data: schema.Store) -> schema.Store:
         logger.error(e)
         raise http_exception
 
+@router.post("/{id}/get", summary="Create an account")
+async def get_store(data: schema.Store) -> schema.Store:
+    http_exception = HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Failed to fetch store.",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    try:
+        store = await models.Store.get_or_none(id=id)
+        return store
+    except Exception as e:
+        logger.error(e)
+        raise http_exception
+
 @router.post("/{id}/update", summary="update store")
 async def upd_store(data: schema.Store) -> schema.Store:
     http_exception = HTTPException(
