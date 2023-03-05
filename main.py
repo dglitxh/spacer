f37252610209070369634aa5faeb6e1c466ae872
ydeezly <ydglitch@gmail.com>
2023-03-05 23:19:28 +0000

    added: store router

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ from tortoise.contrib.pydantic import pydantic_model_creator
 from common.db import init_db, rdb
 from common.logger import logger
 from controllers.user_controller import router as auth_router
+from controllers.store_controller import router as store_router
 
 app = FastAPI()
 app.include_router(auth_router)
+app.include_router(store_router)
 init_db(app)
 
 @app.get("/")
