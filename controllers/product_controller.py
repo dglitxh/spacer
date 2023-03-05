9e639d7ebeb043f7606eea9b99012ba2d4363c57
ydeezly <ydglitch@gmail.com>
2023-03-05 20:44:56 +0000

    add: prod

---
+++ controllers/product_controller.py
@@ -0,0 +1,22 @@
+import os
+import json
+from models import models, schema
+from fastapi import Depends, FastAPI, HTTPException, APIRouter, status
+from dotenv import load_dotenv
+
+router = APIRouter(prefix="/products")
+
+@router.post("/new", summary="Create a product.")
+async def add_product(data: schema.Product) -> schema.Product:
+    http_exception = HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Failed to create store.",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    try:
+        product = await models.Product.create(**data.dict())
+        logger.info("product created succesfully")
+        return product
+    except Exception as e:
+        logger.error(e)
+        raise http_exception
