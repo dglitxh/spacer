6b1d7fbad7d8270456df685e5743ef93cf8bad97
ydeezly <ydglitch@gmail.com>
2023-03-12 11:55:53 +0000

    fix: models

--- models/models.py
+++ models/models.py
@@ -17,60 +17,60 @@ class User(Model):
     def __str__(self):
         return self.name
     class Meta:
-        table: "users"
+        table = "users"
 
 class Store(Model):
-    id: fields.IntField(pk=True, generated=True)
-    name: fields.CharField(max_length=255)
-    description: fields.CharField(max_length=555)
-    category: fields.CharField(max_length=255)
-    cash_total: fields.FloatField()
+    id = fields.IntField(pk=True, generated=True)
+    name = fields.CharField(max_length=255)
+    description = fields.CharField(max_length=555)
+    category = fields.CharField(max_length=255)
+    cash_total = fields.FloatField()
     created_at = fields.DatetimeField(auto_now_add=True)
     modified_at = fields.DatetimeField(auto_now=True)
-    owner: fields.ForeignKeyField(model_name="models.User", related_name="owner")
+    owner = fields.ForeignKeyField(model_name="models.User", related_name="owner")
 
     def __str__(self):
         return self.name
     class Meta:
-        table: "stores"
+        table = "stores"
 
 
 class Product(Model):
-    id: fields.IntField(pk=True, generated=True)
-    name: fields.CharField(max_length=255)
-    description: fields.CharField(max_length=555)
-    category: fields.CharField(max_length=255)
-    price: fields.FloatField()
-    rating: fields.FloatField()
-    store_id: fields.ForeignKeyField(model_name="models.Store", related_name="store")
+    id = fields.IntField(pk=True, generated=True)
+    name = fields.CharField(max_length=255)
+    description = fields.CharField(max_length=555)
+    category = fields.CharField(max_length=255)
+    price = fields.FloatField()
+    rating = fields.FloatField()
+    store_id = fields.ForeignKeyField(model_name="models.Store", related_name="store")
 
     def __str__(self):
         return self.name
     class Meta:
-        table: "products"
+        table = "products"
 
 
 class CartItem(Model):
-    id: fields.IntField(pk=True, generated=True)
-    product: fields.ForeignKeyField(model_name="models.Product")
-    quantity: fields.IntField()
-    total_price: fields.FloatField()
+    id = fields.IntField(pk=True, generated=True)
+    product = fields.ForeignKeyField(model_name="models.Product")
+    quantity = fields.IntField()
+    total_price = fields.FloatField()
 
     def __str__(self):
         return self.name
     class Meta:
-        table: "cart_items"
+        table = "cart_items"
 
 
-class Order(Model): 
-    id: fields.IntField(pk=True, generated=True)
-    owner: fields.ForeignKeyField(model_name="models.User", related_name="user")
-    order_code: fields.UUIDField(generated=True)
-    paid: fields.BooleanField(default=False)
-    delivery: fields.BooleanField(default=False)
-    items: fields.ForeignKeyField(model_name="models.CartItem", related_name="cart_items")
+class Order(Model):
+    id = fields.IntField(pk=True, generated=True)
+    owner = fields.ForeignKeyField(model_name="models.User", related_name="user")
+    order_code = fields.UUIDField(generated=True)
+    paid = fields.BooleanField(default=False)
+    delivery = fields.BooleanField(default=False)
+    items = fields.ForeignKeyField(model_name="models.CartItem", related_name="cart_items")
 
     def __str__(self):
         return self.name
     class Meta:
-        table: "orders"
+        table = "orders"
