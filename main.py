a1c6122edfcfd8b4d673275877187729d28651e5
ydeezly <ydglitch@gmail.com>
2023-03-25 16:36:44 +0000

    upd: main

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ from common.db import init_db, rdb
 from common.logger import logger
 from controllers.user_controller import router as auth_router
 from controllers.store_controller import router as store_router
+from controllers.cart import Cart
 
 app = FastAPI()
 app.include_router(auth_router)
 app.include_router(store_router)
-from .controllers.cart import Cart
+
 
 
 cart = Cart()
