d7cf4c52b8a544f3a4a64507476a8bfba81d2b4f
ydeezly <ydglitch@gmail.com>
2023-03-26 11:41:45 +0000

    fix: prod ctrl

--- controllers/product_controller.py
+++ controllers/product_controller.py
@@ -24,7 +24,7 @@ async def add_product(data: schema.Product) -> schema.Product:
 
 
 @router.get("/{id}", summary="Create a product.")
-async def get_product(data: schema.Product) -> schema.Product:
+async def get_product(id:int) -> schema.Product:
     http_exception = HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="Failed to create Product.",
@@ -37,15 +37,15 @@ async def get_product(data: schema.Product) -> schema.Product:
         logger.error(e)
         raise http_exception
 
-@router.get("/update", summary="Update product.")
-async def upd_product(data: schema.Product) -> schema.Product:
+@router.put("/{id}/update", summary="Update product.")
+async def upd_product(data: schema.Product, id:int) -> schema.Product:
     http_exception = HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Failed to create Product.",
+                detail="Failed to update Product.",
                 headers={"WWW-Authenticate": "Bearer"},
             )
     try:
-        product = await model.Product.get_or_none(id=data.id)
+        product = await models.Product.get_or_none(id=id)
         product.update_from_dict(data.dict(exclude_unset=True))
         await product.save()
         logger.info("Product succesfully updated.")
@@ -56,7 +56,7 @@ async def upd_product(data: schema.Product) -> schema.Product:
 
 
 @router.delete("/{id}/remove", summary="Delete product")
-async def delete_product(data: schema.Product) -> schema.Product:
+async def delete_product(id: int) -> schema.Product:
     http_exception = HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="Failed to delete product.",
@@ -64,7 +64,6 @@ async def delete_product(data: schema.Product) -> schema.Product:
             )
     try:
         product = await models.Product.get_or_none(id=id)
-        product.update_from_dict(data.dict(exclude_unset=True))
         await product.delete()
         logger.info("Product deleted succesfully")
         return product
