cf4eb3d435961bead919181dc3dbbeb3c332f009
ydeezly <ydglitch@gmail.com>
2023-04-20 11:37:55 +0000

    upd: celery

--- common/celery.py
+++ common/celery.py
@@ -7,8 +7,3 @@ celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "amqp://localhost:5
 celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
 celery.autodiscover_tasks()
 
-@celery.task
-def hello():
-    return "hello YD"
-
-hello.delay()
