d1bcc0cd2bd74b66f628404618beb93fdcb9dd5e
ydeezly <ydglitch@gmail.com>
2023-04-21 21:36:35 +0000

    upd: celery

--- common/celery.py
+++ common/celery.py
@@ -5,5 +5,5 @@ from celery import Celery
 celery = Celery("spacer", broker="amqp://localhost:5672")
 celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "amqp://localhost:5672")
 celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
-celery.autodiscover_tasks()
+
 
