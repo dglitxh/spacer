c3f320442ebc6edcf98520f6597ff312ebaefbf9
ydeezly <ydglitch@gmail.com>
2023-04-24 14:27:10 +0000

    fix: celery

--- common/celery.py
+++ common/celery.py
@@ -2,7 +2,7 @@ import os
 from celery import Celery
 
 
-celery = Celery("spacer", broker="amqp://localhost:5672")
+celery = Celery("spacer", broker="amqp://localhost:5672", include=["controllers.tasks"])
 celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "amqp://localhost:5672")
 celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
 
