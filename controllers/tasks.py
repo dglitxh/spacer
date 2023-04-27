faea06e88199096a350c490423f9e21bff6f9027
ydeezly <ydglitch@gmail.com>
2023-04-27 21:24:44 +0000

    add: mailer task

--- controllers/tasks.py
+++ controllers/tasks.py
@@ -1,6 +1,23 @@
 from common.celery import celery
+from fastapi_mail import FastMail, MessageSchema,ConnectionConfig
+
 
 @celery.task
 def hello():
     return "Ydeezzy"
 
+@celery.task
+async def send_mail(template: str, email: str):
+    try:
+        message = MessageSchema(
+            subject="Fastapi-Mail module",
+            recipients=email,
+            body=template,
+            subtype="html"
+            )
+        fm = FastMail(mailer_config)
+        await fm.send_message(message)
+        print(message)
+    except Exception as e:
+        print(e)
+ 
