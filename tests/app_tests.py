da7f523bd953cf2f46cc0ee5b6b56efc1c707e8d
ydeezly <ydglitch@gmail.com>
2023-05-15 22:11:56 +0000

    upd: tests

---
+++ tests/app_tests.py
@@ -0,0 +1,21 @@
+import httpx
+from main import app
+from fastapi.testclient import TestClient
+from common.logger import logger
+from common.db import rdb
+
+prod = {
+    "name": "Big Clothing", 
+    "description": "This is just a piece of clothing",
+    "category": "clothing",
+    "rating": 0.0,
+    "price": 30,
+    "store_id": 1
+}
+
+def test_create_prod():
+    response =  client.post("/products/new", json=prod)
+    json = response.json()
+    logger.info(json)
+    assert response.status_code == 200
+    assert json == prod
