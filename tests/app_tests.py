34d6de6ed1721515705cff166c89ccbe8a03d7d9
ydeezly <ydglitch@gmail.com>
2023-05-16 08:32:44 +0000

    upd: prod tests

--- tests/app_tests.py
+++ tests/app_tests.py
@@ -15,9 +15,29 @@ prod = {
     "store_id": 1
 }
 
+store = {
+    "name": "Big store", 
+    "description": "we sell big tings",
+    "category": "groceries",
+    "cash_total": 20.2,
+    "owner_id": 1
+}
+
 def test_create_prod():
     response =  client.post("/products/new", json=prod)
     json = response.json()
     logger.info(json)
     assert response.status_code == 200
     assert json == prod
+
+def test_get_prod():
+    response = client.get("/products/2/get")
+    json = response.json()
+    logger.info(json)
+    assert response.status_code == 200
+
+def test_get_prod():
+    response = client.get("/products/2/delete")
+    json = response.json()
+    logger.info(json)
+    assert response.status_code == 200
