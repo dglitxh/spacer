30bac24846aa22946e80f3b573d73758933a5417
Ydeezly <74550749+dglitxh@users.noreply.github.com>
2024-03-18 14:53:13 +0000

    Update db.py

--- common/db.py
+++ common/db.py
@@ -34,7 +34,7 @@ def init_db(app):
 
 def init_rdb():
     try:
-        r = redis.Redis(host=rds_url, db=0)
+        r = redis.Redis(host=rds_url,port=6379, decode_responses=True )
         return r
     except Exception as e:
         logger.error("Redis failed to start...")
