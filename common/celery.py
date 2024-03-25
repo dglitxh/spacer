d12e1e4e00919819798073d1310d9ae95d3e1c61
Ydeezly <74550749+dglitxh@users.noreply.github.com>
2024-03-25 06:25:13 -0600

    Update celery.py

--- common/celery.py
+++ common/celery.py
@@ -5,6 +5,6 @@ from .db import rds_url
 
 celery = Celery(main="spacer", broker="amqp://localhost:5672", include=["controllers.tasks"])
 celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "amqp://localhost:5672")
-celery.conf.result_backend = os.environ.get('CELERY_RESULT_BACKEND', "redis://localhost:6379")
+celery.conf.result_backend = os.environ.get('RDS_URL', "redis://localhost:6379")
 
 
